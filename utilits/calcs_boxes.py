a3b30bdd3d913dc41c0389b9da2e286f069dfcb3
ostrebko <oleg.strebkov@gmail.com>
2023-01-21 23:14:04 +0300

    add classes of calc boxes

--- utilits/calcs_boxes.py
+++ utilits/calcs_boxes.py
@@ -33,9 +33,8 @@ class BoxesNums():
     
     def __init__(self, img_path, model_yolov5, config: dict):
         super().__init__()
-        self.folder_name = os.path.basename(os.path.dirname(img_path))
         self.num_boxes = self.calc_boxes(img_path=img_path, 
-                                         model_yolov5=model_yolov5, 
+                                         model_yolov5=model_yolov5,
                                          config=config)
     
 
@@ -47,6 +46,7 @@ class BoxesNums():
         и их число усредняется."""
             
         img_name = os.path.basename(img_path)
+        folder_name = os.path.basename(os.path.dirname(img_path))
         
         original_image = Image.open(img_path)
         max_size = (config.img_size, config.img_size)
@@ -76,9 +76,9 @@ class BoxesNums():
                                         fill = False)
                 ax.add_patch(rect)
             
-            if not os.path.isdir(os.path.join(config.path_to_predicted_images, self.folder_name)):
-                os.mkdir(os.path.join(config.path_to_predicted_images, self.folder_name))
-            save_file = os.path.join(config.path_to_predicted_images, self.folder_name, img_name)
+            if not os.path.isdir(os.path.join(config.path_to_predicted_images, folder_name)):
+                os.mkdir(os.path.join(config.path_to_predicted_images, folder_name))
+            save_file = os.path.join(config.path_to_predicted_images, folder_name, img_name)
             plt.savefig(save_file, bbox_inches='tight', dpi=300)
             plt.close(fig) 
             
@@ -95,45 +95,48 @@ class BoxesNums():
 
 
 
-# class BoxesAvgNums():
-#     """
-#     Decription ...  
-#     Args:
-#     -------------
-#     config (_dict_) - словарь с конфигурацией
-#     ...
-#     """
-#     def __init__(self, model_yolov5, config: dict):
-#         super().__init__()
-#         self.config = config
-#         #self.folder_name = os.path.basename(os.path.dirname(img_path))
-#         #self.model_yolov5 = model_yolov5
-#         #self.img_path = img_path
-#         #self.num_boxes = self.calc_boxes(img_path, model_yolov5)
+class BoxesAvgNums():
+    """
+    Decription ...  
+    Args:
+    -------------
+    config (_dict_) - словарь с конфигурацией
+    ...
+    """
     
-#     def calc_avg_num_blastos(self, path_to_imgs, folder_name, model, is_draw):
-#         start_time = time.time()
-#         imgs_paths = sorted(glob.glob(os.path.join(path_to_imgs, folder_name, '*.jpg')), key=str)      
-#         numb_images = len(imgs_paths)
-#         list_to_calc_avg = []      
-#         with open(os.path.join(
-#             self.config.path_to_predicted_images, folder_name, 'result_' + str(folder_name) + '.csv'), 
-#             'w', newline='') as csvfile:          
-#             for num, imge in enumerate(imgs_paths, start=1):
-#                 print(num, imge)              
-#                 num_blastos = self.calc_boxes(imge, folder_name, model, is_draw)
-#                 print(f'Фото №{num}/{numb_images},', end=' ') 
-#                 list_to_calc_avg.append(num_blastos)              
-#                 csv_writer = csv.writer(csvfile, delimiter=';')
-#                 csv_writer.writerow(list([os.path.basename(imge), num_blastos, 
-#                                         int(num_blastos*0.43)]))  
-#             mean_numb_blasto = int(np.mean(list_to_calc_avg))
-#             print()
-#             print(f'Среднее число бластоспор: {mean_numb_blasto}')
-#             csv_writer.writerow(list(['avrg numb', mean_numb_blasto, 
-#                                     int(mean_numb_blasto*0.43)]))
-#         print('Время расчета: %s seconds' % (time.time() - start_time))
+    def __init__(self, folder_name, model_yolov5, config: dict):
+        super().__init__()
+        self.config = config
+        self.calc_avg_num_blastos(folder_name=folder_name, model=model_yolov5)
+    
 
+    def calc_avg_num_blastos(self, folder_name, model):
+        start_time = time.time()
+
+        imgs_paths = sorted(
+            glob.glob(os.path.join(self.config.path_to_images, folder_name, '*.jpg')), key=str)
+        numb_images = len(imgs_paths)
+        
+        list_to_calc_avg = []      
+        with open(os.path.join(
+            self.config.path_to_predicted_images, folder_name, 'result_' + str(folder_name) + '.csv'), 
+            'w', newline='') as csvfile:
+            for num, imge in enumerate(imgs_paths, start=1):
+                print(num, imge)              
+                #num_blastos = self.calc_boxes(imge, folder_name, model, is_draw)
+                num_blastos = BoxesNums(imge, model, self.config).num_boxes
+                print(f'Фото №{num}/{numb_images},', end=' ') 
+                list_to_calc_avg.append(num_blastos)              
+                csv_writer = csv.writer(csvfile, delimiter=';')
+                csv_writer.writerow(list([os.path.basename(imge), num_blastos, 
+                                        int(num_blastos*0.43)]))  
+            mean_numb_blasto = int(np.mean(list_to_calc_avg))
+            print()
+            print(f'Среднее число бластоспор: {mean_numb_blasto}')
+            csv_writer.writerow(list(['avrg numb', mean_numb_blasto, 
+                                    int(mean_numb_blasto*0.43)]))
+        print('Время расчета: %s seconds' % (time.time() - start_time))
+        #return
 
 
 #     for fold_name in os.listdir(path_to_images):
