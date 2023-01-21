0a96438d7627558a57a90da1e730e04c796fcb1c
ostrebko <oleg.strebkov@gmail.com>
2023-01-21 23:19:15 +0300

    add func of work process for main_py

--- utilits/functions.py
+++ utilits/functions.py
@@ -1,21 +1,17 @@
 # Импортируем библиотеки
-import json
-from dotmap import DotMap
+import os
+import shutil
+from utilits.read_config import config_reader
+from utilits.model_loader import LoadModel
+from utilits.calcs_boxes import BoxesAvgNums
 
-def config_reader(path_to_json_conf: str) -> dict:
-    """Функция загрузки параметров конфигурации в память.
 
-    Args:
-    ------------
-    path_to_json_conf (_str_): путь к файлу конфигурации
-
-    Returns:
-    ------------
-    config (dict): словарь с параметрами конфигурации
-    """    
-    with open(path_to_json_conf, 'r') as config_file:
-        config_dict = json.load(config_file)
-    
-    config = DotMap(config_dict)
-    
-    return config
+def calc_avg_num_blastos(path_to_config): #='config/data_config.json'
+    config = config_reader(path_to_config)
+    model = LoadModel(config).loaded_model
+    for fold_name in os.listdir(config.path_to_images):
+        if os.path.isdir(os.path.join(config.path_to_predicted_images, fold_name)):
+            shutil.rmtree(os.path.join(config.path_to_predicted_images, fold_name))
+        os.mkdir(os.path.join(config.path_to_predicted_images, fold_name))
+        BoxesAvgNums(fold_name, model, config)
+    print('Well done!!!')
