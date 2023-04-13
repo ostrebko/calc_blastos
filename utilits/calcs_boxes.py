97184aa4c99548a84a193b09d17104173ab9dbb3
ostrebko <oleg.strebkov@gmail.com>
2023-04-13 23:50:52 +0300

    add func to calc grid_ver_1

--- utilits/calcs_boxes.py
+++ utilits/calcs_boxes.py
@@ -53,9 +53,9 @@ class BoxesOneImgNums():
         resized_image = original_image.resize(max_size, Image.ANTIALIAS)
         
         boxes = model_yolov5(resized_image, size=config.img_size).xyxy[0][:, :4]
-        
+                
         numb_blasto = len(boxes)
-        print(f'название: {img_name} число бластоспор: {numb_blasto}')
+        print(f'название: {img_name} число бластоспор: {numb_blasto}, оценка в сетке {int(numb_blasto*0.43)}')
         
         if config.is_draw:
             plt.rcParams["figure.figsize"] = (12,8)
@@ -82,20 +82,78 @@ class BoxesOneImgNums():
             plt.savefig(save_file, bbox_inches='tight', dpi=300)
             plt.close(fig) 
             
-            # if not os.path.isdir(os.path.join(config.path_to_predicted_images, self.folder_name)):
-            #     save_file = os.path.join(config.path_to_predicted_images, img_name)
-            #     plt.savefig(save_file, bbox_inches='tight', dpi=300)
-            #     plt.close(fig)
-            # else:
-            #     save_file = os.path.join(config.path_to_predicted_images, self.folder_name, img_name)
-            #     plt.savefig(save_file, bbox_inches='tight', dpi=300)
-            #     plt.close(fig) 
-
         return numb_blasto
+    
+
+    def calc_boxes_in_grid(self, img_path, model_yolov5, config):
+        
+        """Для каждой фото рассчитывается количество бластоспор в границах сетки
+        (предсказание запускается на каждое фото). 
+        Далее предсказанное количество бластоспор собраются в лист 
+        и их число усредняется."""
+            
+        img_name = os.path.basename(img_path)
+        folder_name = os.path.basename(os.path.dirname(img_path))
+        
+        original_image = Image.open(img_path)
+        max_size = (config.img_size, config.img_size)
+        resized_image = original_image.resize(max_size, Image.ANTIALIAS)
+        
+        boxes = np.array(model_yolov5(resized_image, size=config.img_size).xyxy[0][:, :6])
+        
+        sorted_boxes_cls = boxes[boxes[:,-1].argsort()]
+        
+        num_bboxes_in_grid = 0
+
+        if sorted_boxes_cls[0][-1]==0:
+            zero_class = sorted_boxes_cls[0]
+            
+            left_limit_grid = zero_class[0]
+            right_limit_grid = zero_class[2]
+            up_limit_grid = zero_class[1]
+            down_limit_grid = zero_class[3]
+        
+            for bbox in sorted_boxes_cls[1:]:
+                x_c = (bbox[2]+bbox[0])/2
+                y_c = (bbox[3]+bbox[1])/2
+                if (left_limit_grid <= x_c <= right_limit_grid)and(up_limit_grid <= y_c <= down_limit_grid):
+                    num_bboxes_in_grid += 1
+        
+        else:
+            print('сетка не определена')        
 
 
-class BoxesOneFoldNums():
-    pass
+        print(f'название: {img_name} число бластоспор в сетке: {num_bboxes_in_grid}')
+        
+
+        if config.is_draw:
+            plt.rcParams["figure.figsize"] = (12,8)
+            fig, ax = plt.subplots()
+            
+            plt.imshow(resized_image)
+            
+            for item in boxes:
+                width_img = int(item[2] - item[0])
+                height_img = int(item[3] - item[1])
+                ax = plt.gca()
+                rect = patches.Rectangle((item[0],
+                                        item[1]), 
+                                        width_img, 
+                                        height_img, 
+                                        linewidth=1, 
+                                        edgecolor='red', 
+                                        fill = False)
+                ax.add_patch(rect)
+            
+            if not os.path.isdir(os.path.join(config.path_to_predicted_images, folder_name)):
+                os.mkdir(os.path.join(config.path_to_predicted_images, folder_name))
+            save_file = os.path.join(config.path_to_predicted_images, folder_name, img_name)
+            plt.savefig(save_file, bbox_inches='tight', dpi=300)
+            plt.close(fig) 
+            
+            
+        return num_bboxes_in_grid
+
 
 
 class BoxesAvgNums():
